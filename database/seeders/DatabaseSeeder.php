3d99bdced5e3446f29c8d6d1dcf41a48645355b1
hamza-crypto <6793sidique@gmail.com>
2023-01-13 07:13:34 +0500

    Product add from shopify

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,27 +15,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 //        Create Admin
-       $admin = User::create([
-           'name' => 'Hamza Siddique',
-           'email' => '6793siddique@gmail.com',
-           'email_verified_at' => now(),
-           'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-          'role' => 'admin'
-       ]);
+        User::create([
+            'name' => 'Hamza Siddique',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role' => 'admin'
+        ]);
 
-      \App\Models\User::factory(5)->create();
-
-      $stores = [
-          ['name' => 'Ursula Miles', 'slug' => 'ursula-miles', 'token' => 'shpua_da2ffe15517ba8cf9d6d8b46849a369e' ],
-          ['name' => 'Mr Surveillance', 'slug' => 'yoursamour', 'token' => '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' ],
-          ['name' => 'Home Sweet Joy', 'slug' => 'home-sweet-joy', 'token' => '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' ],
-      ];
+        $stores = [
+            ['name' => 'Ursula Miles', 'slug' => 'ursula-miles', 'token' => 'shpua_da2ffe15517ba8cf9d6d8b46849a369e'],
+            ['name' => 'Mr Surveillance', 'slug' => 'yoursamour', 'token' => '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'],
+            ['name' => 'Home Sweet Joy', 'slug' => 'home-sweet-joy', 'token' => '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'],
+        ];
 
         foreach ($stores as $store) {
             \App\Models\Store::create($store);
         }
-
-
 
 
     }
